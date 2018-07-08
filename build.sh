3953cd40f44d73cb7771baf5e42d32ffbce9aacf
jinyeon.choi <jinyeon.choi@navercorp.com>
2018-07-08 23:46:09 +0900

    chore: build setting change;

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ gcc -g -o sample01_scanning sample01_scanning.c -I"/opt/ffmpeg/include" $(pkg-co
 gcc -g -o sample02_demuxing sample02_demuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
 gcc -g -o sample03_remuxing sample03_remuxing.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
 gcc -g -o sample04_decoding sample04_decoding.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil);
+gcc -g -o sample04_decoding sample04_decoding.c -I"/Users/Naver/Documents/vscode/ffmpeg3.2/ffmpeg-2.8.14" $(pkg-config --libs libavformat libavcodec libavutil);
 gcc -g -o sample05_filtering sample05_filtering.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
 gcc -g -o sample06_encoding sample06_encoding.c -I"/opt/ffmpeg/include" $(pkg-config --libs libavformat libavcodec libavutil libavfilter);
